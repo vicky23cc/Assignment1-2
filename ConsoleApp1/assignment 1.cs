16ffd114d97112baee0b0ea6cf748d9b68a04ba2
vicky23cc <yuchu2000123@gmail.com>
2024-02-21 21:28:41 -0500

    Add project files.

---
+++ ConsoleApp1/assignment 1.cs
@@ -0,0 +1,164 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//using System.Diagnostics.Metrics;
+//using static System.Console;
+////playing wish console app
+//Console.WriteLine("what's your favorite color and astrology sign?");
+//string name = Console.ReadLine();
+//Console.WriteLine($"Your hacker name is {name}.");
+
+////practice number size and range
+////1.
+//string[] datatype = { "sbyte", "byte", "short", "ushort", "int", "uint", "long", "ulong", "float", "double", "decimal" };
+//int[] size = { sizeof(sbyte), sizeof(byte), sizeof(short), sizeof(ushort), sizeof(int), sizeof(uint), sizeof(long), sizeof(ulong), sizeof(float), sizeof(double), sizeof(decimal) };
+//double[] minval = { sbyte.MinValue, byte.MinValue, short.MinValue, ushort.MinValue, int.MinValue, uint.MinValue, long.MinValue, ulong.MinValue, float.MinValue, double.MinValue, (double)decimal.MinValue };
+//double[] maxval = { sbyte.MaxValue, byte.MaxValue, short.MaxValue, ushort.MaxValue, int.MaxValue, uint.MaxValue, long.MaxValue, ulong.MaxValue, float.MaxValue, double.MaxValue, (double)decimal.MaxValue };
+
+//Console.WriteLine("{0,-10} {1,5} {2,25} {3,25}\n", "DataType", "size", "Min value", "Max value");
+//for (int conunter = 0; conunter < datatype.Length; conunter++)
+//    Console.WriteLine("{0,-10} {1,5} {2,25} {3,25}", datatype[conunter], size[conunter], minval[conunter], maxval[conunter]);
+
+////2.
+//Console.WriteLine("input the number of centuries?");
+//string val = Console.ReadLine();
+//uint cty = Convert.ToUInt32(val);
+//uint yr = cty * 100;
+//uint dy = cty * 36524;
+//uint hr = cty ^ 876576;
+//uint mins = cty * 52594560;
+//long eds = cty * 3155673600;
+//long mseds = cty * 3155673600000;
+//long micsed = cty * 3155673600000000;
+//ulong nans = cty * 15778454400000000000;
+
+//Console.WriteLine($"{cty} centuries = {yr} years = {dy} days = {hr} hours = {mins} minutes = {eds} seconds = {mseds} milliseconds = {micsed} microseconds = {nans} nanoseconds");
+
+////practive loops and operators
+////1.
+//for (int i = 1; i <= 100; i++)
+//{
+//    if (i % 3 == 0 && i % 5 == 0)
+//    {
+//        Console.WriteLine("FizzBuzz");
+//    }
+//    else if (i % 3 == 0)
+//    {
+//        Console.WriteLine("Fizz");
+//    }
+//    else if (i % 5 == 0)
+//    {
+//        Console.WriteLine("Buzz");
+//    }
+//    else
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+////2. it will be infinity loop.
+
+
+//int max = 500;
+
+//if (max > byte.MaxValue)
+//{
+//    WriteLine("Warning: the max value is greater than maximum value of byte, it would be a infinity loop. The byte range is (0,255)");
+//}
+//else
+//{
+//    for (byte i = 0; i < max; i++)
+//    {
+//        WriteLine(i);
+//    }
+//}
+
+////3.
+//int correctNumber = new Random().Next(3) + 1;
+//Console.WriteLine("Guess the number between 1 and 3");
+//while (true)
+//{
+//    int guessedNumber = int.Parse(Console.ReadLine());
+//    if (guessedNumber > 3 || guessedNumber < 1)
+//    {
+//        Console.WriteLine("the number is not valid, the correct number is between 1 and 3");
+//    }
+//    else if (guessedNumber < correctNumber)
+//    {
+//        Console.WriteLine("the correct number is higher than your guess");
+//    }
+//    else if (guessedNumber > correctNumber)
+//    {
+//        Console.WriteLine("the correct number is less than your guess");
+//    }
+//    else
+//    {
+//        Console.WriteLine("you are correct!");
+//        break;
+//    }
+//}
+
+////4.
+//int rows = 5; // Number of rows in the pyramid
+
+//for (int i = 1; i <= rows; i++)
+//{
+//    // Print spaces before the stars
+//    for (int j = 1; j <= rows - i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+
+//    // Print stars for the current row
+//    for (int k = 1; k <= 2 * i - 1; k++)
+//    {
+//        Console.Write("*");
+//    }
+
+//    Console.WriteLine(); // Move to the next line after printing stars
+//}
+
+////5.
+//DateTime birthDate = new DateTime(2000, 1, 23);
+//TimeSpan age = DateTime.Now - birthDate;
+//int dayage = (int)age.TotalDays;
+//Console.WriteLine($"Your current day age is {dayage}.");
+
+//int daysToNextAnniversary = 10000 - (dayage % 10000);
+//DateTime nextAnniversary = DateTime.Today.AddDays(daysToNextAnniversary);
+//Console.WriteLine($"The next 10,000-day anniversary is on {nextAnniversary:d}.");
+
+////6.
+//DateTime thistime = DateTime.Now;
+//int check = thistime.Hour;
+
+//if (check >= 5 && check < 12)
+//{
+//    Console.WriteLine("Good Morning!");
+//}
+//if (check >= 12 && check < 17)
+//{
+//    Console.WriteLine("Good AfterNoon!");
+//}
+//if (check >= 17 && check < 20)
+//{
+//    Console.WriteLine("Good Evening!");
+//}
+//if (check >= 20 || check < 5)
+//{
+//    Console.WriteLine("Good Night!");
+//}
+
+////7.
+//for (int i = 1; i <= 4; i++)
+//        {
+//            for (int j = 0; j <= 24; j += i)
+//            {
+//                Console.Write(j);
+
+//                if (j < 24 && j + i <= 24)
+//                {
+//                    Console.Write(",");
+//                }
+//            }
+
+//            Console.WriteLine();
+//        }
---
+++ ConsoleApp1/assignment 2.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApp1;
+
+class Program
+{
+
+    //1.
+    //static void Main(string[] args)
+    //{
+    //    // Create the initial array
+    //    int[] originalArray = new int[10];
+
+    //    // Populate the original array with some values
+    //    for (int i = 0; i < originalArray.Length; i++)
+    //    {
+    //        originalArray[i] = i * 2; // Example: Set even numbers in the array
+    //    }
+
+    //    // Print the contents of the original array
+    //    Console.WriteLine("Original Array:");
+    //    PrintArray(originalArray);
+
+    //    // Create a second array with the same length as the original array
+    //    int[] copiedArray = new int[originalArray.Length];
+
+    //    // Copy values from the original array to the new array
+    //    for (int i = 0; i < originalArray.Length; i++)
+    //    {
+    //        copiedArray[i] = originalArray[i];
+    //    }
+
+    //    // Print the contents of the copied array
+    //    Console.WriteLine("\nCopied Array:");
+    //    PrintArray(copiedArray);
+    //}
+
+    //// Method to print the contents of an array
+    //static void PrintArray(int[] array)
+    //{
+    //    foreach (var item in array)
+    //    {
+    //        Console.Write(item + " ");
+    //    }
+    //    Console.WriteLine();
+    //}
+
+
+
+
+
+    ////2.
+    //static void Main(string[] args)
+    //{
+    //    List<string> list = new List<string>();
+
+    //    while (true) {
+    //        Console.WriteLine("Enter an operation (+ to add, - to remove, -- to clear):");
+    //        string input = Console.ReadLine();
+    //        if (string.IsNullOrWhiteSpace(input))
+    //        {
+    //            Console.WriteLine("Invalid input. Please try again.");
+    //            continue;
+    //        }
+    //        char operation = input[0];
+    //        string item = input.Substring(1).Trim();
+    //        if (operation == '+' && item is not null)
+    //        {
+    //            list.Add(item);
+    //            break;
+    //        }
+    //        else if (operation == '-' && item is not null) {
+    //            list.Remove(item);
+    //            break;
+    //        }
+    //        else if (operation == '-' && item is null) {
+    //            list.Clear();
+    //            break;
+    //        }
+    //        else {
+    //            Console.WriteLine("Invalid operation. Please try again.");
+    //        }
+    //        }
+    //    Console.WriteLine("current list contents:");
+    //    foreach (string item in list) {
+    //        Console.WriteLine(item);
+    //    }
+    //}
+
+    //3.
+    //    static void Main(string[] args)
+    //    {
+    //        int start = 1; // Start of the range
+    //        int end = 100; // End of the range
+
+    //        int[] primes = FindPrimesInRange(start, end);
+
+    //        Console.WriteLine("Prime numbers between " + start + " and " + end + ":");
+    //        foreach (int prime in primes)
+    //        {
+    //            Console.Write(prime + " ");
+    //        }
+    //    }
+    //    static int[] FindPrimesInRange(int startNum, int endNum)
+    //    {
+    //        List<int> primelist = new List<int>();
+    //        for (int i = startNum; i <= endNum; i++)
+    //        {
+    //            if (isprime(i))
+    //            {
+    //                primelist.Add(i);
+    //            }
+    //        }
+    //        return primelist.ToArray();
+    //    }
+
+    //    static bool isprime(int i)
+    //    {
+    //        if (i <= 1)
+    //        {
+    //            return true;
+    //        }
+    //        for (int j = 2; j <= Math.Sqrt(i); j++)
+    //        {
+    //            if (i % j == 0)
+    //            {
+    //                return false;
+    //            }
+    //        }
+    //        return true;
+    //    }
+
+    //}
+
+    //4.
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Enter the array of integers (space-separated):");
+    //    int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+    //    Console.WriteLine("Enter the value of k (number of rotations):");
+    //    int k = int.Parse(Console.ReadLine());
+
+    //    int n = array.Length;
+    //    int[] sum = new int[n];
+
+    //    for (int r = 1; r <= k; r++)
+    //    {
+    //        RotateArrayRight(array);
+    //        Console.WriteLine($"After rotation {r}:");
+    //        PrintArray(array);
+
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            sum[i] += array[i];
+    //        }
+    //    }
+
+    //    Console.WriteLine("Final sum array:");
+    //    PrintArray(sum);
+    //}
+
+    //static void RotateArrayRight(int[] array)
+    //{
+    //    int temp = array[array.Length - 1];
+    //    for (int i = array.Length - 1; i > 0; i--)
+    //    {
+    //        array[i] = array[i - 1];
+    //    }
+    //    array[0] = temp;
+    //}
+
+    //static void PrintArray(int[] array)
+    //{
+    //    foreach (int num in array)
+    //    {
+    //        Console.Write(num + " ");
+    //    }
+    //    Console.WriteLine();
+    //}
+
+    // 5.
+    //static void Main(string[] args) {
+    //    Console.WriteLine("Enter the array of integers (space-separated):");
+    //    int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+    //    int counts = 1;
+    //    int longnum = array[0];
+    //    int longestcount = 1;
+
+    //    for (int i = 1; i < array.Length; i++) {
+    //        if (array[i] != array[i - 1])
+    //        {
+    //            counts = 0;
+    //        }
+    //        counts++;
+
+    //        if (counts > longestcount) { 
+    //        longestcount = counts;
+    //        longnum = array[i];
+    //        }
+    //    }
+    //    Console.WriteLine(string.Join(" ", Enumerable.Repeat(longnum,longestcount))) ;
+
+    //}
+
+    //6.
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Enter the array of integers (space-separated):");
+    //    int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+    //    int counts = 1;
+    //    int longnum = array[0];
+    //    int longestcount = 1;
+
+    //    for (int i = 1; i < array.Length; i++)
+    //    {
+    //        if (array[i] != array[i - 1])
+    //        {
+    //            counts = 0;
+    //        }
+    //        counts++;
+
+    //        if (counts > longestcount)
+    //        {
+    //            longestcount = counts;
+    //            longnum = array[i];
+    //        }
+    //    }
+    //    Console.WriteLine($"the number {longnum} is the most frequent (occurs) {longestcount} times ");
+
+    //}
+
+    //practice strings
+    //1.
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Enter the string:");
+    //    string str = Console.ReadLine();
+    //    char[] chars = str.ToCharArray();
+    //    reverseArray(chars);
+    //    PrintArray(chars);
+
+
+
+    //    static void reverseArray(char[] array)
+    //    {
+    //        int range = (array.Length - 1) / 2;
+    //        char temp = 'a';
+    //        for (int i = 0; i <= range; i++)
+    //        {   
+    //            temp = array[i];
+    //            array[i] = array[array.Length - 1 - i];
+    //            array[array.Length - 1 - i] = temp;
+    //        }
+    //    }
+
+    //    static void PrintArray(char[] array)
+    //    {
+    //        foreach (char i in array)
+    //        {
+    //            Console.Write(i);
+    //        }
+    //        Console.WriteLine();
+
+    //    }
+
+    //}
+
+    //2.
+
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Enter a sentence:");
+    //    string sentence = Console.ReadLine();
+
+    //    char[] separators = { '.', ',', ':', ';', '=', '(', ')', '&', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' };
+
+    //    // Split the sentence into words and separators
+    //    string[] wordsAndSeparators = Regex.Split(sentence, @"([.,:;=()&\[\]""'\\/!? ]+)");
+
+    //    string[] words = wordsAndSeparators.Where(word => !string.IsNullOrEmpty(word) && !separators.Contains(word[0])).ToArray();
+    //    string[] originalSeparators = wordsAndSeparators.Where(sep => string.IsNullOrEmpty(sep) || separators.Contains(sep[0])).ToArray();
+
+    //    // Reverse the words
+    //    Array.Reverse(words);
+
+    //    // Merge reversed words with original separators
+    //    string reversedSentence = string.Concat(words.Select((word, index) => word + (index < originalSeparators.Length ? originalSeparators[index] : "")));
+
+
+    //    Console.WriteLine("Reversed sentence:");
+    //    Console.WriteLine(reversedSentence);
+    //}
+
+    //3.
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Enter some text:");
+    //    string text = Console.ReadLine();
+
+    //    // Extract palindromes
+    //    var palindromes = ExtractPalindromes(text);
+
+    //    // Print unique palindromes, sorted
+    //    Console.WriteLine(string.Join(", ", palindromes.Distinct().OrderBy(p => p)));
+    //}
+
+    //static IEnumerable<string> ExtractPalindromes(string text)
+    //{
+    //    // Split the text into words
+    //    string[] words = text.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+    //    // Filter palindromic words
+    //    foreach (string word in words)
+    //    {
+    //        if (IsPalindrome(word))
+    //        {
+    //            yield return word;
+    //        }
+    //    }
+    //}
+
+    //static bool IsPalindrome(string word)
+    //{
+    //    // Check if the word is a palindrome
+    //    int left = 0;
+    //    int right = word.Length - 1;
+    //    while (left < right)
+    //    {
+    //        if (word[left] != word[right])
+    //        {
+    //            return false;
+    //        }
+    //        left++;
+    //        right--;
+    //    }
+    //    return true;
+    //}
+
+    //4.
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an URL:");
+        string url = Console.ReadLine();
+
+        string[] arr = new string[3];
+        arr[0] = url.Contains("://") ? url.Substring(0, url.IndexOf("://")) : ""; // Extract protocol
+
+        int protocolIndex = url.IndexOf("://");
+        if (protocolIndex != -1)
+        {
+            arr[0] = url.Substring(0, protocolIndex); // Extract protocol
+            url = url.Substring(protocolIndex + 3); // Remove protocol part from the URL
+        }
+
+        // Extract server and resource
+        int slashIndex = url.IndexOf('/');
+        if (slashIndex != -1)
+        {
+            arr[1] = url.Substring(0, slashIndex); // Extract server
+            arr[2] = url.Substring(slashIndex + 1); // Extract resource
+        }
+        else
+        {
+            arr[1] = url; // Server is the whole URL
+            arr[2] = ""; // No resource
+        }
+
+        Console.WriteLine($"Protocol: {arr[0]}");
+        Console.WriteLine($"Server: {arr[1]}");
+        Console.WriteLine($"Resource: {arr[2]}");
+    }
+
+}
